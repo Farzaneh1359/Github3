eaf9ef395d21484f288145016ff060d93398820b
farzaneh <farzaneh_bagheri2008@yahoo.com>
2020-06-28 17:48:56 -0500

    Add project files.

---
+++ project3/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        string pattern = @"[d-e]";
+        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+    }
+
+
+    public class Example2b
+    {
+        public static void Main()
+
+
+        {
+            string pattern = @" [d-e][*d]
+";
+            string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(input, pattern, options))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+            }
+        }
+
+        public class Example2c
+        {
+            public static void Main()
+            {
+                string pattern = @" [a][a-z][a]";
+                string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+                RegexOptions options = RegexOptions.Multiline;
+
+                foreach (Match m in Regex.Matches(input, pattern, options))
+                {
+                    Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+                }
+            }
+            public class Example2d
+            {
+                public static void Main()
+                {
+                    string pattern = @" [ ][p][a-z]*";
+                    string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+                    RegexOptions options = RegexOptions.Multiline;
+
+                    foreach (Match m in Regex.Matches(input, pattern, options))
+                    {
+                        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+                    }
+                }
+            }
+            public class Example2e
+            {
+                public static void Main()
+                {
+                    string pattern = @" [a-z]*[e][ ]
+";
+                    string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+                    RegexOptions options = RegexOptions.Multiline;
+
+                    foreach (Match m in Regex.Matches(input, pattern, options))
+                    {
+                        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+                    }
+                }
+            }
+            public class Example
+            {
+                public static void Main()
+                {
+                    string pattern = @" [A-Z]";
+                    string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
+base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
+managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
+space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister
+agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can
+save her people and restore freedom to the galaxy….";
+                    RegexOptions options = RegexOptions.Multiline;
+
+                    foreach (Match m in Regex.Matches(input, pattern, options))
+                    {
+                        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+                    }
+                }
+            }
+        }
+    }
+}
+
